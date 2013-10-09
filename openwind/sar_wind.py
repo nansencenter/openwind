4da916e324d709e5897d03cab1b9907686c51a61
mortenwh <morten.wergeland.hansen@gmail.com>
2013-10-09 16:36:13 +0200

    Decided to resize with eResampleAlg=-1 (average) as this gives the smoothest result, so got rid of many lines of ugly code as well.

--- openwind/sar_wind.py
+++ openwind/sar_wind.py
@@ -47,11 +47,27 @@ class SARWind(Nansat, object):
         if os.path.splitext(self.fileName)[1]=='.nc':
             return
 
-        # Reduce size of image 
+        if 'PixelFunctionType' in self.get_metadata(bandID='sigma0_VV').keys():
+            # copy sigma0_VV to numpy array and add this as a separate band to
+            # avoid problem with resizing pixelfunction bands.
+            # This is a temporary workaround until the nansat resize issue
+            # nansencenter/nansat#41 is fixed
+            s0 = self['sigma0_VV']
+            self.add_band(array=s0, parameters={
+                'wkv': 'surface_backwards_scattering_coefficient_of_radar_wave',
+                'name': 's0_src4wind',
+            })
+        # workaround for incidence angle is needed in any case...
+        inci = self['incidence_angle'] # we probably also have another problem with the FillValue
+        self.add_band(array=inci, parameters={
+            'wkv': 'angle_of_incidence',
+            'name': 'inci_src4wind',
+        })
+        self.vrt = self.raw.copy()
+
+        # Now reduce size of image 
         line_spacing = float(self.get_metadata()['LINE_SPACING'])
-        self.resize(line_spacing/self.pixel_size, eResampleAlg=0) 
-        # save raw vrt to tmp for later retrieval
-        tmp = self.raw
+        self.resize(line_spacing/self.pixel_size) 
         # copy reduced vrt to raw to get correct (reduced) size of the
         # model_wind object created below
         self.raw = self.vrt.copy()
@@ -66,44 +82,14 @@ class SARWind(Nansat, object):
         else:
             winddir = np.ones(np.shape(self[1]))*winddir
     
-        self.raw = tmp
-        # resize self to original size
-        self.resize()
-
-        if 'PixelFunctionType' in self.get_metadata(bandID='sigma0_VV').keys():
-            # copy sigma0_VV to numpy array and add this as a separate band to
-            # avoid problem with resizing pixelfunction bands.
-            # This is a temporary workaround until the nansat resize issue
-            # nansencenter/nansat#41 is fixed
-            s0 = self['sigma0_VV']
-            self.add_band(array=s0, parameters={
-                'wkv': 'surface_backwards_scattering_coefficient_of_radar_wave',
-                'name': 's0tmp',
-            })
-            self.vrt = self.raw.copy()
-
         # NOTE 1: The look direction is defined in the center of the
         # domain clockwise from north. For longer domains, especially at high
         # latitudes, the look direction may vary a lot over the domain, and
         # using the center angle will be a coarse approximation.
         look_direction = float(self.vrt.dataset.GetMetadataItem('SAR_center_look_direction'))
         winddir_relative = np.mod(winddir - look_direction, 360)
-        windspeed, s0, inci = self.get_cmod_wind(winddir_relative)
+        windspeed = self.get_cmod_wind(winddir_relative)
 
-        # Reduce size of self 
-        self.resize(line_spacing/self.pixel_size) 
-        # completely and forever...
-        self.raw = self.vrt.copy()
-
-        # Add source numpy arrays used in cmod as bands
-        self.add_band(array=s0, parameters={
-                        'wkv': 'surface_backwards_scattering_coefficient_of_radar_wave',
-                        'name': 's0_src4wind',
-                    })
-        self.add_band(array=inci, parameters={
-                        'wkv': 'angle_of_incidence',
-                        'name': 'inci_src4wind',
-                    })
         # Add wind speed and direction as bands
         self.add_band(array=windspeed, parameters={
                         'wkv': 'wind_speed',
@@ -112,6 +98,11 @@ class SARWind(Nansat, object):
         self.add_band(array=winddir, parameters={
                         'wkv': 'wind_from_direction',
                         'name': 'winddirection',
+                    })
+        self.add_band(array=winddir_relative, parameters={
+                        'wkv': 'wind_from_direction',
+                        'name': 'winddir_look_relative',
+                        'long_name': 'Wind direction relative to SAR look direction (0 is upwind)',
                     })
 
         # Winddir is defined as the direction from which the wind is blowing
@@ -131,26 +122,19 @@ class SARWind(Nansat, object):
         self.vrt = self.raw.copy()
 
     def get_cmod_wind(self, winddir_relative):
-        # Resize self using Lanczos resampling
-        line_spacing = float(self.get_metadata()['LINE_SPACING'])
-        self.resize(line_spacing/self.pixel_size, eResampleAlg=-1) # averaging
         if 'PixelFunctionType' in self.get_metadata(bandID='sigma0_VV').keys():
             # Temporary workaround of nansencenter/nansat#41
-            s0 = self['s0tmp']
+            s0 = self['s0_src4wind']
         else:
             s0 = self['sigma0_VV']
-        inci = self['incidence_angle']
+        inci = self['inci_src4wind']
         # consider changing cmod to use only one line of incidence angles
         windspeedcmod5 = cmod5n_inverse( s0, winddir_relative,
                 inci )
         windspeedcmod5[np.where(np.isnan(s0))] = np.nan
         windspeedcmod5[np.where(np.isinf(s0))] = np.nan
-        # Resize self back to original
-        self.resize()
 
-        # also returning s0 and inci because these are obtained with a
-        # different eResampleAlg
-        return windspeedcmod5, s0, inci
+        return windspeedcmod5
 
     def plot_example(self, model_wind=None, filename='windfield.png', dir='.'):
 
