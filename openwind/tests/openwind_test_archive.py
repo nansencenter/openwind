686e5b438d95d0badbec6dd2fcddfaa928cc8ce4
mortenwh <morten.wergeland.hansen@gmail.com>
2014-07-08 14:14:02 +0200

    tests updated (run with 'python -m unittest openwind.tests.test_sarwind' from the command line)

--- openwind/tests/openwind_test_archive.py
+++ openwind/tests/openwind_test_archive.py
@@ -7,28 +7,18 @@
 # Modified:	Morten Wergeland Hansen
 #
 # Created:	26.05.2014
-# Last modified:27.06.2014 10:53
+# Last modified:07.07.2014 15:31
 # Copyright:    (c) NERSC
 # License:      
 #-------------------------------------------------------------------------------
 import os
-from test_sarwind import dirname
 
-'''
-    Online datasets
-'''
-asar_agulhas_url = 'ftp://ftp.nersc.no/pub/python_test_data/asar/ASA_WSM_1PNPDE20120327_205532_000002143113_00100_52700_6903.N1'
-fname = os.path.basename(asar_agulhas_url)
-if not os.path.exists(os.path.join(dirname,fname)):
-    os.system('curl -so ' + os.path.join(dirname,fname) + ' ' + asar_agulhas_url )
-asar_agulhas = os.path.join(dirname,fname)
-
-ncep_agulhas_url = 'ftp://ftp.nersc.no/pub/python_test_data/ncep/gfs/gfs20120328/gfs.t00z.master.grbf00'
-fname = os.path.basename(ncep_agulhas_url)
-if not os.path.exists(os.path.join(dirname,fname)):
-    os.system('curl -so ' + os.path.join(dirname,fname) + ' ' + ncep_agulhas_url )
-ncep_agulhas = os.path.join(dirname,fname)
-
+from nansat.tests.nansat_test_archive import TestData
+dirname_test_data = os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)),
+                        'test_data')
+if not os.path.exists(dirname_test_data):
+    os.mkdir(dirname_test_data)
 
 '''
     Local datasets (e.g. Radarsat-2)
@@ -43,54 +33,42 @@ except ImportError as e:
 except IOError as e:
     print e.message
 
-
-
-'''
-    Check existence
-'''
-if not os.path.isfile(asar_agulhas) or not os.path.isfile(ncep_agulhas):
-    asar_agulhas = None
-    ncep_agulhas = None
-    print "Could not access ftp-site with test data"
-
-
 '''
     Test data should be contained in an instance of the TestData class so we're
     able to correctly remove downloaded files after the tests
 '''
-class TestData(object):
-    asar = []
+class OpenWindTestData(TestData):
+
     radarsat2 = []
     ncep4asar = []
-    noData = True
 
     def __init__(self):
-        # OBS: SAR and wind data must be added in pairs for each test
-        if asar_agulhas:
-            self.asar.append(asar_agulhas)
-        if ncep_agulhas:
-            self.ncep4asar.append(ncep_agulhas)
-        # check for data from local archives
-        if 'rs2' in globals() and rs2:
-            self.radarsat2.append(rs2)
-        if 'rs2_quad' in globals() and rs2_quad:
-            self.radarsat2.append(rs2_quad)
+        # OBS: SAR and wind data must be added in parallel
+        super(OpenWindTestData, self).get_asar_agulhas()
+        self.get_ncep_agulhas()
+        self.get_local()
         if self.asar:
             self.noData = False
         if self.radarsat2:
             self.noData = False
 
-    def __del__(self):
-        '''
-            Delete any downloaded files
-        '''
-        if os.path.isfile(asar_agulhas):
-            os.unlink(asar_agulhas)
-        if os.path.isfile(ncep_agulhas):
-            os.unlink(ncep_agulhas)
+    def get_ncep_agulhas(self):
+        ncep_agulhas_url = 'ftp://ftp.nersc.no/pub/python_test_data/ncep/gfs/gfs20120328/gfs.t00z.master.grbf00'
+        fname = os.path.basename(ncep_agulhas_url)
+        ncep_agulhas = os.path.join(dirname_test_data,fname)
+        if not os.path.exists(ncep_agulhas):
+            os.system('curl -so ' + ncep_agulhas + ' ' + ncep_agulhas_url )
+        if not os.path.isfile(ncep_agulhas):
+            ncep_agulhas = None
+            warnings.warn( "Could not access ftp-site with test data - contact " \
+                    "morten.stette@nersc.no to get the ftp-server at NERSC restarted" )
+        else:
+            self.ncep4asar.append(ncep_agulhas)
 
-        self.asar = []
-        self.ncep4asar = []
-        self.radarsat2 = []
-        self.noData = True
-        super(TestData, self).exit()
+    def get_local(self):
+        # check for data from local archives
+        if 'rs2' in globals() and rs2:
+            self.radarsat2.append(rs2)
+        if 'rs2_quad' in globals() and rs2_quad:
+            self.radarsat2.append(rs2_quad)
+
--- openwind/tests/test_sarwind.py
+++ openwind/tests/test_sarwind.py
@@ -7,22 +7,26 @@
 # Modified:	Morten Wergeland Hansen
 #
 # Created:	26.05.2014
-# Last modified:16.06.2014 11:54
+# Last modified:08.07.2014 14:01
 # Copyright:    (c) NERSC
 # License:      
 #-------------------------------------------------------------------------------
 import unittest
 import filecmp
 import os
-dirname = os.path.dirname(__file__)
+import numpy as np
 
 from openwind import *
 from nansat import *
 
 import openwind_test_archive as ota
-test_data = ota.TestData()
+
+dirname_test_plots = os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)), 'plots')
 
 '''
+Downloads online test data, saves it locally and performs tests.
+
 This test suite does not test command line usage. See
 http://hg.python.org/cpython/file/default/Lib/test/test_cmd_line_script.py for
 several examples of how to test handling of command line arguments.
@@ -30,38 +34,50 @@ several examples of how to test handling of command line arguments.
 
 class SARWindTest(unittest.TestCase):
     def setUp(self):
-        if test_data.noData:
+        self.test_data = ota.OpenWindTestData()
+        if self.test_data.noData:
             raise ValueError('No test data available')
 
     def test_sarwind_using_default(self):
-        for i in range(len(test_data.radarsat2)):
-            w = SARWind(test_data.radarsat2[i])
+        for i in range(len(self.test_data.radarsat2)):
+            w = SARWind(self.test_data.radarsat2[i])
             self.assertIsInstance(w, SARWind)
 
     def test_sarwind_using_filenames(self):
-        for i in range(len(test_data.asar)):
-            w = SARWind(test_data.asar[i], wind_direction=test_data.ncep4asar[i])
+        for i in range(len(self.test_data.asar)):
+            w = SARWind(self.test_data.asar[i],
+                    wind_direction=self.test_data.ncep4asar[i])
             w.plot(filename='asar_agulhas_test_plot.png', show=False)
             self.assertIsInstance(w, SARWind)
 
     def test_sarwind_using_asar_filename_ncep_nansat(self):
-        for i in range(len(test_data.asar)):
-            mw = Nansat(test_data.ncep4asar[i])
-            w = SARWind(test_data.asar[i], wind_direction=mw)
+        for i in range(len(self.test_data.asar)):
+            mw = Nansat(self.test_data.ncep4asar[i])
+            w = SARWind(self.test_data.asar[i], wind_direction=mw)
             self.assertIsInstance(w, SARWind)
 
     def test_sarwind_using_asar_nansat_ncep_filename(self):
-        for i in range(len(test_data.asar)):
-            asar = Nansat(test_data.asar[i])
-            w = SARWind(asar, wind_direction=test_data.ncep4asar[i])
+        for i in range(len(self.test_data.asar)):
+            asar = Nansat(self.test_data.asar[i])
+            w = SARWind(asar, wind_direction=self.test_data.ncep4asar[i])
             self.assertIsInstance(w, SARWind)
 
     def test_sarwind_using_asar_nansat_ncep_nansat(self):
-        for i in range(len(test_data.asar)):
-            asar = Nansat(test_data.asar[i])
-            mw = Nansat(test_data.ncep4asar[i])
+        for i in range(len(self.test_data.asar)):
+            asar = Nansat(self.test_data.asar[i])
+            mw = Nansat(self.test_data.ncep4asar[i])
             w = SARWind(asar, wind_direction=mw)
             self.assertIsInstance(w, SARWind)
+
+    def test_nansat_reproject(self):
+        asar = Nansat(self.test_data.asar[0])
+        #asar.resize(pixelsize=500, eResampleAlg=1)
+        asar.resize(0.01)
+        mw = Nansat(self.test_data.ncep4asar[0])
+        import pdb
+        pdb.set_trace()
+        mw.reproject(asar, eResampleAlg=1)
+        self.assertIsInstance(mw[1], np.ndarray)
 
     def test_plot_agulhas(self):
         '''
@@ -70,29 +86,32 @@ class SARWindTest(unittest.TestCase):
 
         If adding more test plots, please make sure the indices are correct
         '''
-        w = SARWind(test_data.asar[0], wind_direction=test_data.ncep4asar[0])
-        w.plot(filename=os.path.join(dirname,
-            'plots/agulhas_test_plot.png'), show=False)
+        w = SARWind(self.test_data.asar[0],
+                wind_direction=self.test_data.ncep4asar[0])
+        w.plot(filename=os.path.join(dirname_test_plots,
+            'agulhas_test_plot.png'), show=False)
         self.assertTrue(filecmp.cmp(
-            os.path.join(dirname,'plots/agulhas_test_plot.png'),
-            os.path.join(dirname,'plots/agulhas_test_plot_ref.png')))
+            os.path.join(dirname_test_plots,'agulhas_test_plot.png'),
+            os.path.join(dirname_test_plots,'agulhas_test_plot_ref.png')))
 
     def test_plot_barents(self):
-        w = SARWind(test_data.radarsat2[0])
-        w.plot(filename=os.path.join(dirname,
-            'plots/rs2_barents_test_plot.png'), show=False)
+        w = SARWind(self.test_data.radarsat2[0])
+        w.plot(filename=os.path.join(dirname_test_plots,
+            'rs2_barents_test_plot.png'), show=False)
         self.assertTrue(filecmp.cmp(
-            os.path.join(dirname,'plots/rs2_barents_test_plot.png'),
-            os.path.join(dirname,'plots/rs2_barents_test_plot_ref.png')))
+            os.path.join(dirname_test_plots,'rs2_barents_test_plot.png'),
+            os.path.join(dirname_test_plots,'rs2_barents_test_plot_ref.png')))
 
     def tearDown(self):
         # Delete test plots
-        if os.path.exists( os.path.join( dirname,
-                'plots/agulhas_test_plot.png' ) ):
-            os.unlink(os.path.join(dirname,'plots/agulhas_test_plot.png'))
-        if os.path.exists( os.path.join( dirname,
-                'plots/rs2_barents_test_plot.png' ) ):
-            os.unlink(os.path.join(dirname,'plots/rs2_barents_test_plot.png'))
+        if os.path.exists( os.path.join( dirname_test_plots,
+                'agulhas_test_plot.png' ) ):
+            os.unlink(os.path.join(dirname_test_plots,'agulhas_test_plot.png'))
+        if os.path.exists( os.path.join( dirname_test_plots,
+                'rs2_barents_test_plot.png' ) ):
+            os.unlink(os.path.join(dirname_test_plots,'rs2_barents_test_plot.png'))
+        ## Remove downloaded test data
+        #self.test_data.delete_downloaded()
 
 
 if __name__=='__main__':
