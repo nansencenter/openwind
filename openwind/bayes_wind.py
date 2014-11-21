15ddd8de23b74cb18e34ccc5a4bba4e998d95ef8
mortenwh <morten.wergeland.hansen@gmail.com>
2014-11-21 16:18:37 +0100

    started Bayesian wind algorithm

---
+++ openwind/bayes_wind.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:		bayes_wind.py
+# Purpose:      
+#
+# Author:       Morten Wergeland Hansen
+# Modified:	Morten Wergeland Hansen
+#
+# Created:	21.11.2014
+# Last modified:21.11.2014 16:17
+# Copyright:    (c) NERSC
+# License:      
+#-------------------------------------------------------------------------------
+
+class BayesianWind():
+    wind_speed_domain = np.linspace(-20,20,41)
