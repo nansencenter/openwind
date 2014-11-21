31f87dc70db1e43434671d7752d1c253534cb048
mortenwh <morten.wergeland.hansen@gmail.com>
2014-11-21 17:10:45 +0100

    created simple cost function

--- openwind/bayes_wind.py
+++ openwind/bayes_wind.py
@@ -6,10 +6,22 @@
 # Modified:	Morten Wergeland Hansen
 #
 # Created:	21.11.2014
-# Last modified:21.11.2014 16:17
+# Last modified:21.11.2014 17:04
 # Copyright:    (c) NERSC
 # License:      
 #-------------------------------------------------------------------------------
+import numpy as np
+from openwind.sar_wind import SARWind
 
-class BayesianWind():
-    wind_speed_domain = np.linspace(-20,20,41)
+def cost_function(dom, obs, err):
+    return np.square(( dom - obs )/err)
+
+
+class BayesianWind(SARWind):
+    wind_speed_domain = np.linspace(-20,20,401)
+    model_wind_uncertainty = 1.5
+
+    def __init__(self):
+        [self.u_domain,self.v_domain] = np.meshgrid(wind_speed_domain,
+                wind_speed_domain)
+
