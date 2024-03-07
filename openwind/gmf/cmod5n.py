1ce595d668511ac2984b373f5ff71d6a3d5b8ab5
Artem Moiseev <artem.moiseev@nersc.no>
2024-03-07 11:30:56 +0100

    fix bool array

--- openwind/gmf/cmod5n.py
+++ openwind/gmf/cmod5n.py
@@ -57,13 +57,20 @@ def cmod5n_forward(v,phi,theta):
 
     GAM=C[ 9]+C[10]*X+C[11]*XX
     S0 =C[12]+C[13]*X
-    
+
     # V is missing! Using V=v as substitute, this is apparently correct
     V=v
     S = A2*V
     S_vec = S.copy() 
-    SlS0 = [S_vec<S0]
-    S_vec[SlS0]=S0[SlS0]
+    # Artem Moiseev (NERSC) 7 Mar 2024: Fixed with removal of the list wrapping
+    # Following lines from original code raise IndexError: too many indices for array: array is 2-dimensional, but 3 were indexed
+    # when executed on python 3.11
+    # SlS0 = [S_vec<S0]
+    # S_vec[SlS0]=S0[SlS0]
+    # This must be related to the way boolean index array is defined with a list wrapping
+    SlS0 = S_vec < S0
+    S_vec[SlS0] = S0[SlS0]
+    
     A3=1./(1.+exp(-S_vec))
     SlS0 = (S<S0)
     A3[SlS0]=A3[SlS0]*(S[SlS0]/S0[SlS0])**( S0[SlS0]*(1.- A3[SlS0]))
@@ -111,11 +118,12 @@ def cmod5n_inverse(sigma0_obs, phi, incidence, iterations=10):
     from numpy import ones, array
     
     # First guess wind speed
-    V = array([10.])*ones(sigma0_obs.shape);
+    V = array([10.])*ones(sigma0_obs.shape)
     step=10.
     
     # Iterating until error is smaller than threshold
     for iterno in range(1, iterations):
+        print(iterno)
         #print iterno
         sigma0_calc = cmod5n_forward(V, phi, incidence)
         ind = sigma0_calc-sigma0_obs>0
